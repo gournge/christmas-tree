8eaad6f149102fa853632c331c851125f5ac4691
gournge <filip.morawiec@gmail.com>
2022-10-05 14:16:42 +0200

    clean up

--- src/tree.cpp
+++ src/tree.cpp
@@ -21,23 +21,15 @@ int main()
     int count = 0;
     int dummy;
     if (if_mp3) play_song();
-    for(int i=0; i<10; i++){
+    while (if_mp3) {
       cout << "hello\n";
       count++; count %= song_length;
       christmas_tree(data, height, width, count%5-2);
       render(data);
       if (not if_mp3) play_buzzer_song(count);
-      // cin >> dummy;
+      Sleep(10000);
+      cin >> dummy;
     }
-    // while (if_mp3) {
-    //   cout << "hello\n";
-    //   count++; count %= song_length;
-    //   christmas_tree(data, height, width, count%5-2);
-    //   render(data);
-    //   if (not if_mp3) play_buzzer_song(count);
-    //   Sleep(10000);
-    //   // cin >> dummy;
-    // }
     cin >> dummy;
     return 0;
 }

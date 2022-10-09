a07762867722b10a83ff2d7c202b05c980de8a25
gournge <filip.morawiec@gmail.com>
2022-10-09 14:44:01 +0200

    tree remake

--- include/fns.hpp
+++ include/fns.hpp
@@ -2,13 +2,12 @@
 using namespace std;
 
 void render(vector<vector<int>>& data);
+void clearscreen(vector<vector<int>>& data);
 
-void row(vector<vector<int>>& data, int x, int y, int fill);
-
+void row(vector<vector<int>>& data, int x, int y, int fill, int type);
 void triangle(vector<vector<int>>& data, int x, int y, int size, int cut);
-
 void christmas_tree(vector<vector<int>>& data, int height, int width, int shift);
+void lights(vector<vector<int>>& data);
 
 void play_song();
-
 void play_buzzer_song(int moment);
--- src/tree.cpp
+++ src/tree.cpp
@@ -6,30 +6,32 @@ using namespace std;
 
 int main()
 {
-    int height, width;  // height is the amount of triangles
-                        // width is their size
+    int n, s;  // n is the amount of triangles
+                        // s is their size
     bool if_mp3 = false;
-    cout << "enter height and width (of trees) consecutively:\n";
-    cin >> height >> width;
+    cout << "enter n and s (of trees) consecutively:\n";
+    cin >> n >> s;
     cout << "enter whether to play mp3 music (1 or 0)\n";
     cin >> if_mp3;
 
-    vector<vector<int>> data( height*width , vector<int> (height*width, 1));
+    vector<vector<int>> data( n*s , vector<int> (n*s, 0));
 
-    int song_length = 45;
+
+
+    const int song_length = 45;
 
     int count = 0;
-    int dummy;
     if (if_mp3) play_song();
-    while (if_mp3) {
-      cout << "hello\n";
+    while (1) {
       count++; count %= song_length;
-      christmas_tree(data, height, width, count%5-2);
+
+      christmas_tree(data, n+1, 2*s, abs(count%11-5)-2);
+      // for (auto &l : data) { for (auto &t : l) cout << t; }
       render(data);
+      Sleep(1000);
+      clearscreen(data);
       if (not if_mp3) play_buzzer_song(count);
-      Sleep(10000);
-      cin >> dummy;
+
     }
-    cin >> dummy;
     return 0;
 }
